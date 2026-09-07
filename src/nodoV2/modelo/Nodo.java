da9db7ac99b06fb1d1077818ffb2eba9be06614e
juanchicoyunda1234 <62398115+juanchicoyunda1234@users.noreply.github.com>
2026-09-06 22:50:36 -0500

    Crear Clase nodo

--- src/nodoV2/modelo/Nodo.java
+++ src/nodoV2/modelo/Nodo.java
@@ -1,4 +1,27 @@
 package nodoV2.modelo;
 
 public class Nodo {
+    private int numero ;
+    private Nodo siguiente;
+
+    public Nodo(int numero, Nodo siguiente) {
+        this.numero = numero;
+        this.siguiente = siguiente;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
 }
